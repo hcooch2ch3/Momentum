774751032b1ba979bff8aabcb77ea40e7448d8ab
SM <hcooch2ch3@gmail.com>
2021-09-05 00:34:02 +0900

    Feat all

---
+++ js/background.js
@@ -0,0 +1,7 @@
+const images = ["1.jpg", "2.jpg", "3.jpg"];
+
+const chosenImage = images[Math.floor(Math.random()* images.length)];
+
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "100%";
